e38312fbd7e4e3fbace6fca17275871a05cb36e3
Tadeo <domenechtadeo@outlook.es>
2022-01-26 17:55:07 +0100

    Added the naviagtion with login and logout

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/marvel", {
+      replace: true,
+    });
+  };
+
   return (
-    <div>
+    <div className="container mt-5">
       <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 };
