8be27fabbb9d6f1762aeaca867ef13cddb62f969
Tadeo <domenechtadeo@outlook.es>
2022-02-22 09:50:32 +0100

    Create the public Route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,13 +4,21 @@ import { Login } from "../components/Login/Login";
 
 import { DashboardRoutes } from "./DashboardRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
           <Route
             path="/*"
             element={
