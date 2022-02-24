96187a277f7539b7f1ecc1ced421a7d6b1588ebb
Tadeo <domenechtadeo@outlook.es>
2022-02-24 11:21:18 +0100

    Create test for Search component

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,7 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ReactTestRenderer from "react-test-renderer";
 import { MemoryRouter } from "react-router-dom";
 import { Search } from "./Search";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 describe("Give a Search component", () => {
   test("Should rendered correcty", () => {
@@ -24,5 +32,47 @@ describe("Give a Search component", () => {
     const searchText = screen.getByText("Search Hero");
 
     expect(searchText).toBeInTheDocument();
+  });
+
+  test("Should have show a Batman and input with this value", () => {
+    render(
+      <MemoryRouter initialEntries={["/search?q=batman"]}>
+        <Search />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByPlaceholderText("Search hero");
+
+    expect(input.value).toBe("batman");
+  });
+
+  test("It should show error if not found hero", () => {
+    render(
+      <MemoryRouter initialEntries={["/search?q=batman123"]}>
+        <Search />
+      </MemoryRouter>
+    );
+
+    const expectText = screen.getByText("Don't have results: batman123");
+
+    expect(expectText).toBeInTheDocument();
+  });
+
+  describe("When click a button with info in input", () => {
+    test("Then it should call the function handleSubmit", () => {
+      render(
+        <MemoryRouter initialEntries={["/search"]}>
+          <Search />
+        </MemoryRouter>
+      );
+
+      const input = screen.getByPlaceholderText("Search hero");
+      const button = screen.getByRole("button");
+
+      userEvent.type(input, "Batman");
+      userEvent.click(button);
+
+      expect(mockNavigate).toHaveBeenCalledWith("?q=Batman");
+    });
   });
 });
